929274589f74ec312e4c4509845b00cb514f04af
i-grantastic <igrantchapman@gmail.com>
2025-09-27 13:02:00 -0400

    Long name test

--- src/index.js
+++ src/index.js
@@ -344,7 +344,7 @@ client.on('interactionCreate', async (interaction) => {
       const sniperShortName = sniper.displayName.split(' ')[0];
       const snipedShortName = sniped.displayName.split(' ')[0];
       const medal = medals[index] || `(${index+1})`;
-      leaderboard += `${medal} ${sniperShortName} ⇄ ${snipedShortName} (${count}) \n`;
+      leaderboard += `${medal} ${sniper} ⇄ ${snipedShortName} (${count}) \n`;
     };
 
     // create the EmbedBuilder
